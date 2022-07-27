b48233a38dd5fa6af9abc99c345f6dd207ec741e
Abstract-X <abstract-x-mail@protonmail.com>
2022-07-27 19:13:40 +0300

    Add docstrings to `Exception_`

--- xcept/exception.py
+++ xcept/exception.py
@@ -9,10 +9,29 @@ from xcept.warnings import MissingFieldWarning, UnknownFieldWarning
 
 @dataclass
 class Exception_(Exception):  # noqa
+    """Base class for custom non-exit exceptions.
+
+    The behavior is mostly inherited from the built-in Exception class.
+    The difference is that this class provides more convenience when
+    working with custom exception attributes, and also allows you to
+    create verbose messages using standard formatting syntax.
+
+    If for some reason you don't need to include all attributes
+    in a message, define ALL_REPLACEMENT_FIELDS_IS_REQUIRED = False
+    to disable checks and warnings.
+    """
     ALL_REPLACEMENT_FIELDS_IS_REQUIRED = True
     _template: str = field(repr=False, metadata={"is_template": True})
 
     def __post_init__(self):
+        """Initialize an object.
+
+        If a template does not contain all replacement fields and
+        attribute ALL_REPLACEMENT_FIELDS_IS_REQUIRED is True, the
+        xcept.warnings.MissingFieldWarning occurs.
+        If a template contains unknown replacement fields, the
+        xcept.warnings.UnknownFieldWarning occurs.
+        """
         self._fields = {
             i.name
             for i in dataclasses.fields(self)
@@ -33,9 +52,11 @@ class Exception_(Exception):  # noqa
         )
 
     def __str__(self):
+        """Return an exception message."""
         return self.get_message()
 
     def get_message(self) -> str:
+        """Return an exception message."""
         arguments = {
             name: value
             for name, value in vars(self).items()
